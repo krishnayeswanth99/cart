34e363147f115a5078c01c13e4b47c076e491fd9
Krishna Yeswanth <krishnayeswanth99@gmail.com>
2021-05-09 23:00:39 +0530

    MEAN Stack

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './entities/product.model';
+import { StatusService } from './running/status.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'cart';
+export class AppComponent implements OnInit{
+  title = 'Angular-Node-Cart';
+  status = "DOWN";
+  createdProduct!: IProduct;
+
+  constructor(protected statusService: StatusService){}
+
+  ngOnInit(){
+    this.statusService.getStatus()
+          .then((res: any)=>{
+            this.status=res.status;
+          });
+  }
+  onCreatedProduct(createdProduct: IProduct){
+    this.createdProduct = createdProduct;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCreateComponent } from './product/product-create/product-create.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCreateComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
